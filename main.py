f98d6a25bd80395a79f8cd801bb121ae242f8d82
Mahmed235 <mahmed@kellettschool.com>
2024-11-06 04:43:04 +0000

    save

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # 
+
+from random import randint
+correct = randint(16,64)
+number = None
+while number != correct:
+
+    print("Guess my number!!!")
+    number = int(input())
+    if number > correct:
+        print("LOWER")
+    if number < correct:
+        print("HIGHER")
+    if number == correct:
+        print("YOU WON WOOOHOO")
+
+
+five_less = correct - 5 gv
+five_more = correct + 5
+print("ALMOST WITHIN 5")
